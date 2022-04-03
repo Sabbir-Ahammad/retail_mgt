d742ce4d9f959e86d1bfb046b7e6d6523120ec62
Sabbir-Ahammad <94116363+Sabbir-Ahammad@users.noreply.github.com>
2022-04-03 11:21:46 +0600

    security

--- src/main/java/com/spring/boot/SecurityConfig.java
+++ src/main/java/com/spring/boot/SecurityConfig.java
@@ -1,5 +1,38 @@
 package com.spring.boot;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.passwordEncoder(passwordEncoder())
+			.withUser("user")
+			.password(passwordEncoder().encode("1234"))
+			.roles("ADMIN", "USER");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().fullyAuthenticated()
+			.and()
+				.csrf()
+				.disable() /* to stop server to through 403 error fr controller post method */
+			.httpBasic();
+	}
 }
